b2441f36a59a0edde1c35dde0e91e36c40a00db5
Weidong Lian <weidong.lian@gmail.com>
2020-05-01 13:17:30 +0200

    shoe example

--- src/shoe.rs
+++ src/shoe.rs
@@ -1,6 +1,35 @@
-struct Shoe {
-    size: u32,
-    style: String,
+#[derive(PartialEq, Clone, Debug)]
+pub struct Shoe {
+    pub size: u32,
+    pub style: String,
 }
 
-fn shoe_in_my_size() {}
+pub fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|x| x.size >= shoe_size).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn filter_by_size() {
+        let shoes = vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker"),
+            },
+            Shoe {
+                size: 20,
+                style: String::from("sandal"),
+            },
+            Shoe {
+                size: 30,
+                style: String::from("boot"),
+            },
+        ];
+
+        let myshoes = shoes_in_my_size(shoes.clone(), 25);
+        assert_eq!(myshoes.as_slice(), &shoes[2..=2]);
+    }
+}
