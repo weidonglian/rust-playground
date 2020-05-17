edd17917ce5cca01f1fd87eec09a436763d47d7b
Weidong Lian <weidong.lian@gmail.com>
2020-05-17 23:41:40 +0200

    thread and error handling

--- tests/error_handling_test.rs
+++ tests/error_handling_test.rs
@@ -20,8 +20,28 @@ fn iter_over_results() {
     let strings = vec!["tofu", "93", "18"];
     let numbers = strings
         .into_iter()
-        .map(|s| s.parse::<i32>())
-        .filter_map()
+        .filter_map(|s| s.parse::<i32>().ok())
         .collect::<Vec<_>>();
+    assert_eq!(&numbers, &[93, 18]);
     println!("Results: {:?}", numbers);
+}
+
+#[test]
+fn iter_over_results_fail_all() {
+    let strings = vec!["93", "tofu", "18"];
+    let numbers: Result<Vec<_>, _> = strings.into_iter().map(|s| s.parse::<i32>()).collect();
+    assert!(numbers.is_err());
+}
+
+#[test]
+fn result_type() {
+    #[derive(Debug)]
+    enum MyError {
+        A,
+        B,
+    };
+
+    let e1: Result<i32, MyError> = Err(MyError::A);
+    let e2 = Err::<i32, _>(MyError::B);
+    println!("err is {:?} and {:?}", e1, e2);
 }
