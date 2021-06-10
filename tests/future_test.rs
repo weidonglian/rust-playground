a766f13524511aa14d2ec75848c8267482027271
Weidong Lian <weidong.lian@gmail.com>
2021-06-10 13:08:16 +0200

    add customized future

--- tests/future_test.rs
+++ tests/future_test.rs
@@ -1,15 +1,31 @@
-use std::time::Instant;
+use std::time::{Instant, Duration};
 use std::future::Future;
+use std::task::{Context, Poll};
+use std::pin::Pin;
 
 struct Delay {
     when: Instant,
 }
 
-impl Delay for Future {
+impl Future for Delay {
+    type Output = &'static str;
 
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        if Instant::now() >= self.when {
+            println!("Delay is triggering...");
+            Poll::Ready("done")
+        } else {
+            cx.waker().wake_by_ref();
+            Poll::Pending
+        }
+    }
 }
-#[test]
-fn delay_future() {
-    let start = Instant::now();
 
+#[tokio::test]
+async fn delay_future() {
+    let start = Instant::now();
+    let when = start + Duration::from_millis(10);
+    Delay { when }.await;
+    println!("Time taken: {:?}", Instant::now() - start);
+    assert!(Instant::now() >= when);
 }
